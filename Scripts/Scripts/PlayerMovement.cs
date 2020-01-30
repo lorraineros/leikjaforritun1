9df43372543d5e87aef2c363c9a7600674d497bb
lorraineros <43746757+lorraineros@users.noreply.github.com>
2020-01-30 23:20:12 +0000

    ok

---
+++ Scripts/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	public float forwardForce = 2000f;
+	public float sidewaysForce = 500f;
+
+	void FixedUpdate ()
+	{
+		// fer áfram
+		rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+		if (Input.GetKey("d"))
+		{
+			// Fer til hægri
+			rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+
+		if (Input.GetKey("a")) // ef ýtt er á takkan "a"
+		{
+			// fer til vinstri
+			rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+
+		if (rb.position.y < -1f)
+		{
+			FindObjectOfType<GameManager>().EndGame();
+		}
+	}
+}
