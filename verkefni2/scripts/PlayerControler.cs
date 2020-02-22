de054fbada8c6062b9664be19f6f01cec714571f
lorraineros <43746757+lorraineros@users.noreply.github.com>
2020-02-22 02:44:37 +0000

    leikur2

---
+++ verkefni2/scripts/PlayerControler.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerControler : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        countText.text = "Teljari: " + count.ToString();
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        float jump;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            jump = 10f;
+        else
+            jump = 0;
+
+        Vector3 movement = new Vector3 (moveHorizontal, jump, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Teljari: " + count.ToString();
+        if(count >= 10)
+        {
+            winText.text = "borð staðist";
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
