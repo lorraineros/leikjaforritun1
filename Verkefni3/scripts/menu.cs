d406859ba283e33b53bd88f0fca4322e79726ba0
lorraineros <43746757+lorraineros@users.noreply.github.com>
2020-03-23 18:34:39 +0000

    verkefni 3

--- Verkefni3/scripts/menu.cs
+++ Verkefni3/scripts/menu.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class menu : MonoBehaviour
 {
+    // Next scene birtist
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
